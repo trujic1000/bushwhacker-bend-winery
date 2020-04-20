7cd2aeef4aa5286ec88035157c37f013b214be0b
Aleksandar Trujic <aleksandar.trujic@live.com>
2020-04-19 22:56:42 -0500

    contact import fixed

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,6 @@ import Landing from "./landing";
 import FeaturedWines from "./featuredWines";
 import AboutUs from "./aboutUs";
 import Gallery from "./gallery";
-import Contact from "./Contact";
+import Contact from "./contact";
 
 export { Landing, FeaturedWines, AboutUs, Gallery, Contact };
